5b65922b1128b9800b175f0d9d9285b6a2617909
xjl2019401197 <2550908862@qq.com>
2023-03-14 00:01:21 +0800

    3-13

--- src/main/java/com/example/wbsystem_ssm/security/AuthenticationEntryPointImpl.java
+++ src/main/java/com/example/wbsystem_ssm/security/AuthenticationEntryPointImpl.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -11,7 +12,7 @@ import java.io.Serializable;
 
 /**
  * 认证失败处理类 返回未授权
- * 
+ *
  * @author ruoyi
  */
 @Component
@@ -21,14 +22,12 @@ public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint, S
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e)
-            throws IOException
-    {
+            throws IOException, ServletException {
         String msg = "请求访问：{}，认证失败，无法访问系统资源"+ request.getRequestURI().toString();
             response.setStatus(200);
             response.setContentType("application/json");
             response.setCharacterEncoding("utf-8");
-            response.getWriter().print(msg);
-        response.sendRedirect(request.getContextPath() + "unauth.html");
+        request.getRequestDispatcher("404.html").forward(request,response);
 
     }
 }
